e60090601b533fb73f0a98cc0821c5902b265799
kazu8901 <still.500ml@gmail.com>
2021-09-15 11:02:45 +0900

    フッターコンポーネント化、マテリアルUI導入

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # typeNext
+
+## 参考にした構築
+https://qiita.com/kurosame/items/68159bf1c900915bdba1
+https://zenn.dev/stin/scraps/ec71e0bfde8973
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/ress.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
